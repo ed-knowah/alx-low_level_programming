2c2b462b954044c2745fcb22adf9640de35d9cbb
ed-knowah <edetnoah@gmail.com>
2022-03-11 14:59:06 +0100

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Prints the alphabet.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char s_alp[26] = "abcdefghijklmnopqrstuvwxyz";
+char l_alp[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i, j;
+for (i = 0; i < 26; i++);
+{
+putchar(s_alp[i]);
+}
+for (j = 0; j < 26; j++)
+{
+   putchar(l_alp[j]);
+}
+putchar('\n');
+return (0);
+}

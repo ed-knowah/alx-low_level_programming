df7792f644089cbee8854aa6afe320dc4262da76
ed-knowah <edetnoah@gmail.com>
2022-03-11 10:22:07 +0100

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - positive or negative function.
+*
+*Return: (success)
+*/
+int main(void)
+{
+char alphabet;
+putchar((alphabet).tolower());
+return (0);
+}
